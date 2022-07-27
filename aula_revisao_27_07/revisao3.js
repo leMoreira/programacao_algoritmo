c687dbbae04679fee6044adfc21e070d8e25f12a
Leandro Moreira <leandromoreira.dev@gmail.com>
2022-07-27 08:26:21 -0300

    Criando aula 3 utilizando bibliotecas nativa do javascript

---
+++ aula_revisao_27_07/revisao3.js
@@ -0,0 +1,13 @@
+// Média do aluno
+let aluno = "Beltrano Oliveira Ramamex";
+let nota_1 = 9.5;
+let nota_2 = 9.5;
+let nota_3 = 2.5;
+let nota_4 = 7.0;
+
+let media = (nota_1 + nota_2 + nota_3 + nota_4) / 4;
+console.log(`Aluno: ${aluno}`);
+console.log(`Média: ${media.toFixed(1)}`);
+console.log(' ❤️ '.repeat(50));
+console.log(Math.round(media));
+
