8ac2658dbc0c6f39578b64be660d0608aa2d66e3
Leandro Moreira <leandromoreira.dev@gmail.com>
2022-07-27 08:00:30 -0300

    criei dois arquivos javascript. 1 - Variáveis, 2- Soma

---
+++ aula_revisao_27_07/revisao1.js
@@ -0,0 +1,8 @@
+var numero = 1; // inteiro (integer)
+var nome = "Lele"; // // Varchar, String, Text
+var preco = 245.90; // decimal, float, double
+var passouDeAno = true ; // Boolean 
+
+let nomeVariavel = "Lele, na aula de Javascript";
+
+console.log(preco);
---
+++ aula_revisao_27_07/revisao2.js
@@ -0,0 +1,9 @@
+let numero_1 = 40;
+let numero_2 = 20;
+
+let soma = numero_1 + numero_2;
+
+console.log("O valor da soma "+ soma);
+console.log('*'.repeat(30));
+console.log(`O valor da soma ${soma}`);
+
