c0a9f3949cd646727fe52375ba9ba95ef186f2a7
Leandro Moreira <leandromoreira.dev@gmail.com>
2022-08-03 09:20:14 -0300

    Aprendendo while com node simples

---
+++ laco_repeticao/while/basic.js
@@ -0,0 +1,27 @@
+console.log("0");
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+console.log('-'.repeat(20));
+
+// Laço de petição while 
+
+let contador = 0;
+while(contador <= 5){
+    console.log(contador);
+    contador ++;
+}
+
+console.log('*'.repeat(30));
+
+let i = 5;
+while(i >=0){
+    console.log(i);
+    i--;
+}
+
+
+
---
+++ laco_repeticao/while/basic_2.js
@@ -0,0 +1,7 @@
+let j = 0;
+
+while(j <= 100){
+    console.log(`n - ${j}`);
+
+    j = j + 2;
+}
